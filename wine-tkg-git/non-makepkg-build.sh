e96733cfa8729855f72875b86ffeb8f3e54d797f
Shimizu Hikaru <MuPendulum@users.noreply.github.com>
2024-11-24 17:09:31 -0500

    wine: customize

--- wine-tkg-git/non-makepkg-build.sh
+++ wine-tkg-git/non-makepkg-build.sh
@@ -39,7 +39,7 @@
 #
 # ===========================================================================================================================================================================
 
-pkgname=wine-tkg
+pkgname=wine-mu
 _build_in_tmpfs="true"
 _esyncsrcdir='esync'
 _where="$PWD" # Track the base directory as different Arch-based distros are moving srcdir around
